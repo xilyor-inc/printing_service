fc65819bfaa654be93e9735bc7becfe8dbac5524
mohamedelleuch777 <usher7med@gmail.com>
2024-02-26 04:02:52 +0300

    interface is ready only server is missing

--- src/eposprintservice/app.py
+++ src/eposprintservice/app.py
@@ -1,37 +1,72 @@
 import toga
 from toga.style import Pack
-from toga.style.pack import COLUMN, ROW
+from toga.style.pack import COLUMN, ROW, CENTER, LEFT, RIGHT
 from .print_lib import Printer
 
 
 class ePosPrintService(toga.App):
+    main_window = None
     printer = Printer()
+    input_ip= None
+    input_port= None
+    server_indicator= None
 
     def startup(self):
+
+        address_box = toga.Box()
+        port_box = toga.Box()
+        buttons_box = toga.Box()
         main_box = toga.Box()
 
-        # Create buttons
-        btn_connect = toga.Button('Connect to Network Printer', on_press=self.connect)
-        btn_print = toga.Button('Print Test Text', on_press=self.print_test_text)
+        # IP address input field
+        ip_label = toga.Label('IP Address:')
+        self.input_ip = toga.TextInput(value='192.168.1.37', placeholder='Enter The Printer IP')
+        # Port input field
+        port_label = toga.Label('Port:')
+        self.input_port = toga.TextInput(value='9100', placeholder='Enter The Printer Port')
 
-        # Add the buttons to the main box
-        main_box.add(btn_connect)
-        main_box.add(btn_print)
+        # Buttons
+        btn_connect = toga.Button('Connect to Printer', on_press=self.connect_printer)
+        btn_disconnect = toga.Button('Disconnect', on_press=self.disconnect_printer)
+        btn_test_printer = toga.Button('Test Printer', on_press=self.print_test_text)
+        btn_start_server = toga.Button('Start Server', on_press=self.start_server)
+        self.server_indicator = toga.Label("OFF")
+
+        # Add widgets to the main box
+        address_box.add(ip_label)
+        address_box.add(self.input_ip)
+        port_box.add(port_label)
+        port_box.add(self.input_port)
+        buttons_box.add(btn_connect)
+        buttons_box.add(btn_disconnect)
+
+        main_box.add(address_box)
+        main_box.add(port_box)
+        main_box.add(buttons_box)
+        main_box.add(btn_test_printer)
+        main_box.add(btn_start_server)
+        main_box.add(self.server_indicator)
+
+        main_box.style.update(direction=COLUMN, padding=10)
+        address_box.style.update(direction=ROW, padding=5)
+        port_box.style.update(direction=ROW, padding=5)
+        buttons_box.style.update(direction=ROW, padding=5)
+
+        self.input_ip.style.update(width=220, padding=5)
+        self.input_port.style.update(width=100, padding=5)
+        ip_label.style.update(width=100, padding=5, alignment=CENTER)
+        port_label.style.update(width=100, padding=5, alignment=CENTER)
 
         # Add the main box to the main window
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = main_box
         self.main_window.show()
 
-    def show_alert(self, widget):
-        # Display an alert when the button is clicked
-        self.main_window.info_dialog('Alert', 'Button clicked! hoho')
-
-    def connect(self, widget):
+    def connect_printer(self, widget):
         print("Connect to Network Printer")
         # Get IP and port from input fields
-        ip = '192.168.1.33'  # self.ip_input.value
-        port = 9100  # self.port_input.value
+        ip = self.input_ip.value
+        port = int(self.input_port.value)
 
         # Connect to the network printer
         self.printer.connect_printer(ip, port)
@@ -41,9 +76,22 @@ class ePosPrintService(toga.App):
             self.main_window.error_dialog('Error', 'Failed to connect to printer!')
 
     def print_test_text(self, widget):
-        self.printer.print_text('Hello, World!')
+        text = "Hello World!\n"
+        self.printer.set_bold(True)
+        self.printer.print_text("Bold: "+text)
+        self.printer.set_bold(False)
+        self.printer.print_text("Normal: "+text)
+        self.printer.set_font('a')
+        self.printer.print_text("Font A: "+text)
+        self.printer.set_font('b')
+        self.printer.print_text("Font B: "+text)
         self.printer.cut_paper()
+
+    def disconnect_printer(self, widget):
         self.printer.close_printer()
+
+    def start_server(self, widget):
+        pass
 
 
 def main():
--- src/eposprintservice/print_lib.py
+++ src/eposprintservice/print_lib.py
@@ -1,4 +1,6 @@
 from escpos.printer import Network
+from PIL import Image
+import io
 
 
 class Printer:
@@ -28,3 +30,69 @@ class Printer:
 
     def close_printer(self):
         self.printer.close()
+
+    def set_font(self, font):
+        """
+        Set the font style for the text.
+        Possible values for font: 'a', 'b', 'c'.
+        """
+        self.printer.set(font=font)
+
+    def set_size(self, width, height):
+        """
+        Set the font size for the text.
+        Parameters:
+        - width: Width of the font (1 to 8).
+        - height: Height of the font (1 to 8).
+        """
+        self.printer.set(width=width, height=height)
+
+    def set_align(self, align):
+        """
+        Set the alignment of the text.
+        Possible values for align: 'left', 'center', 'right'.
+        """
+        self.printer.set(align=align)
+
+    def set_bold(self, bold=True):
+        """
+        Set the bold style for the text.
+        Parameters:
+        - bold: Boolean value indicating whether to enable bold style (default: True).
+        """
+        self.printer.set(bold=bold)
+
+    def set_underline(self, underline=True):
+        """
+        Set the underline style for the text.
+        Parameters:
+        - underline: Boolean value indicating whether to enable underline style (default: True).
+        """
+        self.printer.set(underline=underline)
+
+    def set_reverse(self, reverse=True):
+        """
+        Set the reverse color style for the text.
+        Parameters:
+        - reverse: Boolean value indicating whether to enable reverse color style (default: True).
+        """
+        self.printer.set(reverse=reverse)
+
+    def print_image(self, image_data):
+        if not self.is_connected:
+            return False
+
+        try:
+            # Load image from binary data
+            image = Image.open(io.BytesIO(image_data))
+
+            # Convert image to monochrome bitmap
+            width, height = image.size
+            image = image.convert('1')  # Convert to 1-bit monochrome bitmap
+
+            # Send image data to the printer
+            self.printer.image(image, width, height)
+            return True
+        except Exception as e:
+            print(f"Error printing image: {e}")
+            return False
