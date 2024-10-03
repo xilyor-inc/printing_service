780321b59c33a62b48cc9145e534d49a6bb3e251
mohamedelleuch777 <usher7med@gmail.com>
2024-10-03 13:44:08 +0100

    not working

--- server.py
+++ server.py
@@ -28,6 +28,113 @@ class PrintServer:
                     return jsonify({"error": "connection failed"}), 500
             return jsonify({"error": "invalid data"}), 400
 
+
+        @app.route('/disconnect_printer', methods=['POST'])
+        def connect_printer():
+            data = request.json
+            ip = data.get('ip')
+            port = data.get('port')
+            if ip and port:
+                is_connected = cls.printer.connect_printer(ip, port)
+                if is_connected:
+                    return jsonify({"status": "connected"}), 200
+                else:
+                    return jsonify({"error": "connection failed"}), 500
+            return jsonify({"error": "invalid data"}), 400
+
+
+        @app.route('/print', methods=['POST'])
+        def print():
+            content_length = int(self.headers['Content-Length'])
+            post_data = self.rfile.read(content_length)
+            json_data = json.loads(post_data.decode('utf-8'))
+
+            # Print the received JSON data
+            print(json_data)
+
+            for i in range(len(json_data)):
+                item = json_data[i]
+
+                print("Table Size:", len(json_data), "Item:", i)
+                print(item)
+
+                # Execute the appropriate method based on the command
+                if item.startswith("PRINT:"):
+                    command, text = item.split(':', 1)
+                    self.printer.print_text(text)
+                elif item.startswith("TABLE:"):
+                    command, cols_width = item.split(':', 1)
+                    cols_width = cols_width.split(',')  # example 20,8,8
+                    # Convert each number from string to integer using map() and list comprehension
+                    cols_width = [int(num_str) for num_str in cols_width]
+                    table = []
+                    table_finished = False
+                    while not table_finished and i < len(json_data):
+                        i = i + 1
+                        item = json_data[i]
+                        if item == "TABLE_END":
+                            i = i + 1
+                            table_finished = True
+                        else:
+                            # split the item by comma
+                            row = item.split(',')
+                            table.append(row)
+
+                    # print the table after has been constructed
+                    self.printer.print_table(table, cols_width)
+                    print("Table printed was: ", table)
+                elif item == "NEW_LINE":
+                    self.printer.print_text("\n")
+                elif item == "CENTER":
+                    self.printer.set_align("center")
+                elif item == "LEFT":
+                    self.printer.set_align("left")
+                elif item == "RIGHT":
+                    self.printer.set_align("right")
+                elif item == "CUT_PAPER":
+                    self.printer.cut_paper()
+                elif item == "CLOSE_PRINTER":
+                    self.printer.close_printer()
+                elif item == "SET_FONT_A":
+                    self.printer.set_font('a')
+                elif item == "SET_FONT_B":
+                    self.printer.set_font('b')
+                elif item == "SET_SIZE":
+                    cmd, width, height = map(int, item.split(','))
+                    self.printer.set_size(width, height)
+                elif item == "SET_BOLD":
+                    self.printer.set_bold(True)
+                elif item == "UNSET_BOLD":
+                    self.printer.set_bold(False)
+                elif item == "SET_UNDERLINE":
+                    self.printer.set_underline(2)
+                elif item == "UNSET_UNDERLINE":
+                    self.printer.set_underline(0)
+                elif item == "SET_INVERTED":
+                    self.printer.set_inverted(True)
+                elif item == "UNSET_INVERTED":
+                    self.printer.set_inverted(False)
+                elif item.startswith("WIDTH:"):
+                    command, text = item.split(':', 1)
+                    self.printer.set_width(int(text))
+                elif item.startswith("HEIGHT:"):
+                    command, text = item.split(':', 1)
+                    self.printer.set_height(int(text))
+                elif item == "TABLE_END":
+                    pass
+                elif item == "PRINT_IMAGE":
+                    pass
+                    # self.printer.print_image(text)
+                else:
+                    print("Unknown command:", item)
+
+            # Send a response
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.send_header("Access-Control-Allow-Origin", "*")  # Allow requests from any origin
+            self.end_headers()
+            self.wfile.write(b'Received POST request')
+
         @app.route('/print_test', methods=['GET'])
         def print_test():
             if cls.printer:
