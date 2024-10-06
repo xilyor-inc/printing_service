2fbff5fc8518091286ee5cc962738bca742a4411
mohamedelleuch777 <usher7med@gmail.com>
2024-10-06 23:27:42 +0100

    flask is working

--- server.py
+++ server.py
@@ -3,157 +3,121 @@ from print_lib import Printer
 from flask_cors import CORS  # Import the CORS extension
 
 
-class PrintServer:
-    printer = None
-
-    @classmethod
-    def set_printer(cls, printer_instance):
-        cls.printer = printer_instance
-
-    @classmethod
-    def create_app(cls):
-        app = Flask(__name__)
-        CORS(app)  # Enable CORS for all routes
-
-        @app.route('/connect_printer', methods=['POST'])
-        def connect_printer():
-            data = request.json
-            ip = data.get('ip')
-            port = data.get('port')
-            if ip and port:
-                is_connected = cls.printer.connect_printer(ip, port)
-                if is_connected:
-                    return jsonify({"status": "connected"}), 200
-                else:
-                    return jsonify({"error": "connection failed"}), 500
-            return jsonify({"error": "invalid data"}), 400
+app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
 
-        @app.route('/disconnect_printer', methods=['POST'])
-        def connect_printer():
-            data = request.json
-            ip = data.get('ip')
-            port = data.get('port')
-            if ip and port:
-                is_connected = cls.printer.connect_printer(ip, port)
-                if is_connected:
-                    return jsonify({"status": "connected"}), 200
-                else:
-                    return jsonify({"error": "connection failed"}), 500
-            return jsonify({"error": "invalid data"}), 400
+@app.route('/', methods=['GET'])
+def nik_ommk():
+    return jsonify({"status": "ok"})
 
 
-        @app.route('/print', methods=['POST'])
-        def print():
-            content_length = int(self.headers['Content-Length'])
-            post_data = self.rfile.read(content_length)
-            json_data = json.loads(post_data.decode('utf-8'))
+printer = Printer()
 
-            # Print the received JSON data
-            print(json_data)
 
-            for i in range(len(json_data)):
+@app.route('/connect_printer', methods=['POST'])
+def connect_printer():
+    data = request.json
+    ip = data.get('ip')
+    port = data.get('port')
+    if ip and port:
+        is_connected = printer.connect_printer(ip, port)
+        if is_connected:
+            return jsonify({"status": "connected"}), 200
+        else:
+            return jsonify({"error": "connection failed"}), 500
+    return jsonify({"error": "invalid data"}), 400
+
+
+
+@app.route('/disconnect_printer', methods=['POST'])
+def disconnect_printer():
+    printer.close_printer()
+    return jsonify({"status": "disconnected successfully"}), 200
+
+@app.route('/print', methods=['POST'])
+def print__():
+
+    json_data = request.json
+
+    for i in range(len(json_data)):
+        item = json_data[i]
+
+        print("Table Size:", len(json_data), "Item:", i)
+        print(item)
+
+        # Execute the appropriate method based on the command
+        if item.startswith("PRINT:"):
+            command, text = item.split(':', 1)
+            printer.print_text(text)
+        elif item.startswith("TABLE:"):
+            command, cols_width = item.split(':', 1)
+            cols_width = cols_width.split(',')  # example 20,8,8
+            # Convert each number from string to integer using map() and list comprehension
+            cols_width = [int(num_str) for num_str in cols_width]
+            table = []
+            table_finished = False
+            while not table_finished and i < len(json_data):
+                i = i + 1
                 item = json_data[i]
-
-                print("Table Size:", len(json_data), "Item:", i)
-                print(item)
-
-                # Execute the appropriate method based on the command
-                if item.startswith("PRINT:"):
-                    command, text = item.split(':', 1)
-                    self.printer.print_text(text)
-                elif item.startswith("TABLE:"):
-                    command, cols_width = item.split(':', 1)
-                    cols_width = cols_width.split(',')  # example 20,8,8
-                    # Convert each number from string to integer using map() and list comprehension
-                    cols_width = [int(num_str) for num_str in cols_width]
-                    table = []
-                    table_finished = False
-                    while not table_finished and i < len(json_data):
-                        i = i + 1
-                        item = json_data[i]
-                        if item == "TABLE_END":
-                            i = i + 1
-                            table_finished = True
-                        else:
-                            # split the item by comma
-                            row = item.split(',')
-                            table.append(row)
-
-                    # print the table after has been constructed
-                    self.printer.print_table(table, cols_width)
-                    print("Table printed was: ", table)
-                elif item == "NEW_LINE":
-                    self.printer.print_text("\n")
-                elif item == "CENTER":
-                    self.printer.set_align("center")
-                elif item == "LEFT":
-                    self.printer.set_align("left")
-                elif item == "RIGHT":
-                    self.printer.set_align("right")
-                elif item == "CUT_PAPER":
-                    self.printer.cut_paper()
-                elif item == "CLOSE_PRINTER":
-                    self.printer.close_printer()
-                elif item == "SET_FONT_A":
-                    self.printer.set_font('a')
-                elif item == "SET_FONT_B":
-                    self.printer.set_font('b')
-                elif item == "SET_SIZE":
-                    cmd, width, height = map(int, item.split(','))
-                    self.printer.set_size(width, height)
-                elif item == "SET_BOLD":
-                    self.printer.set_bold(True)
-                elif item == "UNSET_BOLD":
-                    self.printer.set_bold(False)
-                elif item == "SET_UNDERLINE":
-                    self.printer.set_underline(2)
-                elif item == "UNSET_UNDERLINE":
-                    self.printer.set_underline(0)
-                elif item == "SET_INVERTED":
-                    self.printer.set_inverted(True)
-                elif item == "UNSET_INVERTED":
-                    self.printer.set_inverted(False)
-                elif item.startswith("WIDTH:"):
-                    command, text = item.split(':', 1)
-                    self.printer.set_width(int(text))
-                elif item.startswith("HEIGHT:"):
-                    command, text = item.split(':', 1)
-                    self.printer.set_height(int(text))
-                elif item == "TABLE_END":
-                    pass
-                elif item == "PRINT_IMAGE":
-                    pass
-                    # self.printer.print_image(text)
+                if item == "TABLE_END":
+                    i = i + 1
+                    table_finished = True
                 else:
-                    print("Unknown command:", item)
+                    # split the item by comma
+                    row = item.split(',')
+                    table.append(row)
 
-            # Send a response
-            self.send_response(200)
-            self.send_header('Content-type', 'text/plain')
-            self.send_header("Access-Control-Allow-Origin", "*")  # Allow requests from any origin
-            self.end_headers()
-            self.wfile.write(b'Received POST request')
+            # print the table after has been constructed
+            printer.print_table(table, cols_width)
+            print("Table printed was: ", table)
+        elif item == "NEW_LINE":
+            printer.print_text("\n")
+        elif item == "CENTER":
+            printer.set_align("center")
+        elif item == "LEFT":
+            printer.set_align("left")
+        elif item == "RIGHT":
+            printer.set_align("right")
+        elif item == "CUT_PAPER":
+            printer.cut_paper()
+        elif item == "CLOSE_PRINTER":
+            printer.close_printer()
+        elif item == "SET_FONT_A":
+            printer.set_font('a')
+        elif item == "SET_FONT_B":
+            printer.set_font('b')
+        elif item == "SET_SIZE":
+            cmd, width, height = map(int, item.split(','))
+            printer.set_size(width, height)
+        elif item == "SET_BOLD":
+            printer.set_bold(True)
+        elif item == "UNSET_BOLD":
+            printer.set_bold(False)
+        elif item == "SET_UNDERLINE":
+            printer.set_underline(2)
+        elif item == "UNSET_UNDERLINE":
+            printer.set_underline(0)
+        elif item == "SET_INVERTED":
+            printer.set_inverted(True)
+        elif item == "UNSET_INVERTED":
+            printer.set_inverted(False)
+        elif item.startswith("WIDTH:"):
+            command, text = item.split(':', 1)
+            printer.set_width(int(text))
+        elif item.startswith("HEIGHT:"):
+            command, text = item.split(':', 1)
+            printer.set_height(int(text))
+        elif item == "TABLE_END":
+            pass
+        elif item == "PRINT_IMAGE":
+            pass
+            # printer.print_image(text)
+        else:
+            print("Unknown command:", item)
 
-        @app.route('/print_test', methods=['GET'])
-        def print_test():
-            if cls.printer:
-                cls.printer.print_text("This is a test page")
-                return jsonify({"status": "test page printed"}), 200
-            return jsonify({"error": "printer not connected"}), 400
+    # Send a response
+    return jsonify({"status": "ok"}), 200
 
-        @app.route('/print_table', methods=['GET'])
-        def print_table():
-            if cls.printer:
-                cls.printer.print_text("Printing test table")
-                return jsonify({"status": "test table printed"}), 200
-            return jsonify({"error": "printer not connected"}), 400
-
-        return app
-
-# Uncomment this to run the Flask server directly from this script (optional)
-# if __name__ == "__main__":
-#     printer = Printer()
-#     PrintServer.set_printer(printer)
-#     app = PrintServer.create_app()
-#     app.run(host="0.0.0.0", port=8000 )
+# app = PrintServer.create_app()
+app.run(host="0.0.0.0", port=8000)
